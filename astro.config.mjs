9afb95b3744b7c8c5239a8cad6cd96ed44df6a9f
Gero24 <okgero@gmail.com>
2024-01-28 19:26:24 -0300

    term

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://cosmosastros.netlify.app/"
+  site: "https://cosmosastros.netlify.app/",
+  integrations: [preact()]
 });
